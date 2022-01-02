d960df3afb7b2a6094c924877fe54ff018196b2a
shotanue <shotanue@gmail.com>
2022-01-02 21:55:59 +0900

    fix comment

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,10 @@
-/// 書籍を見るとOOPっぽく書きたくなるが、
-/// 取りうるデータ構造を列挙してmatch式で分岐させた方がrustっぽいらしい。
-/// 書籍のtokenフィールドはenumで代用できるため不要になる。
+/// The parser chapter guides me to write the code in OOP,
+/// but it is easier to write and understand the code in Rust's ways,
+/// enumerating the data structure and matching expression.
+
+/// Adhering to the parser chapter requires a token field.
+/// Still, I ignored creating it because Rust's enum can represent the Let statement,
+/// Return statement, or Expression statement, even without the token field.
 #[derive(Debug)]
 pub enum Statement {
     LET { name: Expression, value: Expression },
