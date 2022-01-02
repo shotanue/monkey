790ca72034d8e342a2e405a0e697920938a8fe8c
shotanue <shotanue@gmail.com>
2022-01-03 00:15:55 +0900

    構文解析のデバッグを簡単にするため、fmt::Displayを実装

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::Formatter;
+
 /// The parser chapter guides me to write the code in OOP,
 /// but it is easier to write and understand the code in Rust's ways,
 /// enumerating the data structure and matching expression.
@@ -9,6 +12,7 @@
 pub enum Statement {
     LET { name: Expression, value: Expression },
     RETURN(Expression),
+    EXPRESSION(Expression),
 }
 
 #[derive(Debug)]
@@ -18,4 +22,45 @@ pub enum Expression {
 
 pub struct Program {
     pub statements: Vec<Statement>,
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Statement::LET { name, value } => write!(f, "let {} = {};", name, value),
+            Statement::RETURN(x) => write!(f, "return {}", x),
+            Statement::EXPRESSION(x) => write!(f, "{}", x),
+        }
+    }
+}
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Identifier(x) => write!(f, "{}", x),
+        }
+    }
+}
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for statement in self.statements.iter() {
+            writeln!(f, "{}", statement).unwrap();
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ast::{Expression, Program, Statement};
+
+    #[test]
+    fn test_string() {
+        let program = Program {
+            statements: vec![Statement::LET {
+                name: Expression::Identifier("myVar".to_string()),
+                value: Expression::Identifier("anotherVar".to_string()),
+            }],
+        };
+        assert_eq!("let myVar = anotherVar;\n", program.to_string())
+    }
 }
